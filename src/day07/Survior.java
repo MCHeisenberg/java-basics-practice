c88455de7fcf698cd7697daef76889ea0ea905fd
hs <1519706548@qq.com>
2026-03-18 16:57:19 +0800

    feat(entity): 新增 Survivor 废土幸存者实体类
    1. 定义 name、hp、caps 私有属性
    2. 提供全参构造方法完成出厂初始化
    3. 按需提供对外暴露的 Getter 和 Setter 接口

---
+++ src/day07/Survior.java
@@ -0,0 +1,37 @@
+package day07;
+
+public class Survior {
+    private String name;
+    private int hp;
+    private int caps;
+
+    public Survior(String name, int hp,int caps){
+        this.name = name;
+        this.hp = hp;
+        this.caps = caps;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHp(){
+        return this.hp;
+    }
+
+    public int getCaps(){
+        return this.caps;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+
+    public void setCaps(int caps){
+        this.caps = caps;
+    }
+}
