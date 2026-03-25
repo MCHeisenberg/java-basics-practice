bba4d89e084dd7a6b64866edd5be20867321c3c9
hs <1519706548@qq.com>
2026-03-26 00:45:27 +0800

    add: practice -addScoreByName() mechod

--- src/day13/Main.java
+++ src/day13/Main.java
@@ -4,49 +4,52 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<User> users=new ArrayList<User>();
-        users.add(new User("nama",74));
-        users.add(new User("namb",372));
-
-        int count=0;
-        count=countHighScore(users);
-        System.out.println("count"+count);
-
-        User foundUser = findByName(users,"nama");
-        foundUser.show();
-
-        User maxUser = findMaxScoreUser(users);
-        maxUser.show();
+        ArrayList<User> users = new ArrayList<User>();
+        users.add(new User("nama",2345));
+        users.add(new User("namb",465));
+        addScoreByName(users,"nama",354);
+        showAll(users);
     }
-    public static User findMaxScoreUser(ArrayList<User> users){
-        if(users.size()==0){
-            return null;
-        }
-        User maxUser=users.get(0);
-        for(int i=1;i<=users.size()-1;i++){
-           if(users.get(i).score>maxUser.score){
-               maxUser = users.get(i);
-           }
-        }
-        return maxUser;
-
-    }
-    public static User findByName(ArrayList<User> users,String name){
-        for(User ghost:users){
+    public static User findByName(ArrayList<User> users, String name){
+        for(User ghost : users){
             if(ghost.name.equals(name)){
                 return ghost;
             }
         }
         return null;
+
     }
-    public static int countHighScore(ArrayList<User> users){
-        int count=0;
+
+    public static void addScoreByName(ArrayList<User> users, String name , int value){
+        User foundUser = findByName(users,name);
+        if(foundUser != null){
+            foundUser.score += value;
+        }
+    }
+
+    public static void showAll(ArrayList<User> users){
         for(User ghost: users){
-            if(ghost.score>=60){
-                count ++;
+            ghost.show();
+        }
+    }
+
+    public static User findMaxScoreUser(ArrayList<User> users) {
+        if (users.size() == 0) {
+            return null;
+        }
+
+        User maxUser = users.get(0);
+
+        for (int i = 1; i < users.size(); i++) {
+            if (users.get(i).score > maxUser.score) {
+                maxUser = users.get(i);
             }
         }
-        return count;
+
+        return maxUser;
     }
 }
+
+
+
 
