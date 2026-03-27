0786f09d15e2711521bee3f705354c0413599b44
hs <1519706548@qq.com>
2026-03-27 22:27:31 +0800

    add: practice B1~7

--- src/day14/Main.java
+++ src/day14/Main.java
@@ -13,62 +13,147 @@ public class Main {
 
     }
     public static void showAll(ArrayList<User> users){
-        for(User ghost : users){
+        for(User ghost:users){
             ghost.show();
         }
     }
+
     public static User findByName(ArrayList<User> users,String name){
-        for(User ghost: users){
+        for(User ghost : users){
             if(ghost.name.equals(name)){
                 return ghost;
             }
         }
         return null;
     }
-    public static int countHighScore(ArrayList<User> users){
-        int count = 0;
-        for(User ghost:users){
-            if(ghost.score>=60){
-                count ++;
-            }
-        }
-        return count;
-    }
+
     public static User findMaxScoreUser(ArrayList<User> users){
-        if(users.size()==0){
+        if(users.size()== 0){
             return null;
         }
         User maxUser = users.get(0);
-        for(int i=1;i<=users.size()-1;i++){
-            if(users.get(i).score>maxUser.score){
-                maxUser = users.get(i);
+        for(User ghost : users){
+            if(ghost!=null && ghost.score>maxUser.score){
+                maxUser = ghost;
             }
         }
         return maxUser;
     }
-    public static void addScoreByName(ArrayList<User> users,String name,int value){//$$$$$$$$$$value
-        User foundUser = findByName(users,name);
-        if(foundUser != null){
-            foundUser.score += value;
-        }
-    }
 
-    public static void removeByName(ArrayList<User> users,String name){
-        for(int i = 0;i<users.size();i++){
-            if(users.get(i).name == name){
-                users.remove(i);
-                return;//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-            }
-        }
-    }
 
-    public static void updateScoreByName(ArrayList<User> users,String name,int newScore){
-        for(int i = 0;i<users.size();i++){
-            if(users.get(i).name == name){
-                users.get(i).score = newScore;
-                return;
-            }
-        }
-    }
+
+
+
+//    public static void showAll(ArrayList<User> users){
+//        for(User ghost :users){
+//            ghost.show();
+//        }
+//    }
+//    public static User findByName(ArrayList<User> users , String name){
+//        for(User ghost:users){
+//            if(ghost.name.equals(name)){
+//                return ghost;
+//            }
+//        }
+//        return null;
+//    }
+//    public static int countHighScore(ArrayList<User> users){
+//        int count = 0;
+//        for(User ghost:users){
+//            count ++;
+//        }
+//        return count;
+//    }
+//
+//    public static User findMaxScoreUser(ArrayList<User> users){
+//        if(users.size()==0){
+//            return null;
+//        }
+//        User maxUser = users.get(0);
+//        for(int i=1;i<=users.size()-1;i++){
+//            if(users.get(i).score>maxUser.score){
+//                maxUser = users.get(i);
+//            }
+//        }
+//        return maxUser;
+//    }
+//
+//    public static void addScoreByName(ArrayList<User> users,String name , int value){
+//        User foundUser = findByName(users, name);
+//            if(foundUser != null){
+//                foundUser.score += value;
+//            }
+//    }
+//
+//    public static void updateScoreByName(ArrayList<User> users, String name,int newScore){
+//        User foundUser = findByName(users,name);
+//        if(foundUser!=null){
+//            foundUser.score=newScore;
+//        }
+//    }
+
+
+
+//----------------------------
+
+
+//    public static void showAll(ArrayList<User> users){
+//        for(User ghost : users){
+//            ghost.show();
+//        }
+//    }
+//    public static User findByName(ArrayList<User> users,String name){
+//        for(User ghost: users){
+//            if(ghost.name.equals(name)){
+//                return ghost;
+//            }
+//        }
+//        return null;
+//    }
+//    public static int countHighScore(ArrayList<User> users){
+//        int count = 0;
+//        for(User ghost:users){
+//            if(ghost.score>=60){
+//                count ++;
+//            }
+//        }
+//        return count;
+//    }
+//    public static User findMaxScoreUser(ArrayList<User> users){
+//        if(users.size()==0){
+//            return null;
+//        }
+//        User maxUser = users.get(0);
+//        for(int i=1;i<=users.size()-1;i++){
+//            if(users.get(i).score>maxUser.score){
+//                maxUser = users.get(i);
+//            }
+//        }
+//        return maxUser;
+//    }
+//    public static void addScoreByName(ArrayList<User> users,String name,int value){//$$$$$$$$$$value
+//        User foundUser = findByName(users,name);
+//        if(foundUser != null){
+//            foundUser.score += value;
+//        }
+//    }
+//
+//    public static void removeByName(ArrayList<User> users,String name){
+//        for(int i = 0;i<users.size();i++){
+//            if(users.get(i).name == name){
+//                users.remove(i);
+//                return;//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//            }
+//        }
+//    }
+//
+//    public static void updateScoreByName(ArrayList<User> users,String name,int newScore){
+//        for(int i = 0;i<users.size();i++){
+//            if(users.get(i).name == name){
+//                users.get(i).score = newScore;
+//                return;
+//            }
+//        }
+//    }
 
 }
