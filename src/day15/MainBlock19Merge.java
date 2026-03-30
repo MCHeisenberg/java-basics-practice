5c4668c79ec0026d809e47f07cd9da987469d400
hs <1519706548@qq.com>
2026-03-30 23:57:21 +0800

    add: block 19 merge

--- src/day15/MainBlock19Merge.java
+++ src/day15/MainBlock19Merge.java
@@ -1,5 +1,52 @@
 package day15;
 
 public class MainBlock19Merge {
+    public static void main(String[] args) {
+        
+    }
+    public static int sumArray(int[] nums) {
+        int sum=0;
+        for (int i=0;i<nums.length;i++) {
+            sum+=nums[i];
+        }
+        return sum;
+    }
+    
+    public static int findMax(int[] nums) {
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+               max =  nums[i];
+            }
+        }
+        return max;
+    }
 
+    public static boolean hasNumber(int[] nums, int target) {
+
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int countTarget(int[] nums,int target) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==target){
+                count ++;
+            }
+        }
+
+        return count;
+    }
+    public static void replaceTarget(int[] nums, int oldValue, int newValue) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == oldValue) {
+                nums[i] = newValue;
+            }
+        }
+    }
 }
